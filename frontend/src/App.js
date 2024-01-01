b5aaace863659e20043c26ac479bd306df5a5554
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2024-01-01 17:13:03 +0100

    Form styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from "react";
 import api from "./components/apiConfig";
+import { useTheme, createTheme, ThemeProvider } from "@mui/material/styles";
+
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  Button,
+  Card,
+  Grid,
+  CardContent,
+  Container,
+  CssBaseline,
+} from "@mui/material";
 import "./App.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   const [algorithms, setAlgorithms] = useState([]);
-  const [testingFunctions, setTestingFunctions] = useState([]);
+  const [fitnessFunctions, setFitnessFunctions] = useState([]);
+  const [selectedAlgorithm, setSelectedAlgorithm] = useState(null);
+  const [selectedFitnessFunction, setSelectedFitnessFunction] = useState([]);
+  const theme = useTheme();
 
   const fetchAlgorithms = async () => {
     api
@@ -20,14 +44,14 @@ function App() {
         ]);
       });
   };
-  const fetchTestingFunctions = async () => {
+  const fetchFitnessFunction = async () => {
     api
       .get("http://localhost:3001/api/get")
       .then((response) => {
         console.log(response);
       })
       .catch((error) => {
-        setTestingFunctions([
+        setFitnessFunctions([
           { name: "f1", id: 0 },
           { name: "f2", id: 1 },
           { name: "f3", id: 2 },
@@ -35,55 +59,107 @@ function App() {
       });
   };
 
-  const addAlgorithm = async () => {
-    const result = await api.post("http://localhost:3001/api/insert", {
-      name: "a4",
-    });
-    console.log(result);
-  };
-
   const renderAlgorithms = () => {
-    return algorithms.map((algorithm) => {
-      return (
-        <div className="algorithm" key={algorithm.id}>
-          <h2>
-            {algorithm.name}
-            <input type="checkbox" />
-          </h2>
-        </div>
-      );
-    });
-  };
-  const renderTestingFunctions = () => {
-    return testingFunctions.map((testingFunction) => {
-      return (
-        <div className="testingFunction" key={testingFunction.id}>
-          <h2>
-            {testingFunction.name}
-            <input type="checkbox" />
-          </h2>
-        </div>
-      );
-    });
+    return (
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>Algorithms</FormLabel>
+            <RadioGroup
+              value={selectedAlgorithm}
+              onChange={(event) => {
+                setSelectedAlgorithm(event.target.value);
+              }}
+            >
+              {algorithms.map((algorithm) => {
+                return (
+                  <FormControlLabel
+                    value={algorithm.id}
+                    control={<Radio />}
+                    label={algorithm.name}
+                  />
+                );
+              })}
+            </RadioGroup>
+          </FormControl>
+        </CardContent>
+      </Card>
+    );
   };
 
+  const renderFitnessFunctions = () => {
+    return (
+      <Card>
+        <CardContent>
+          <FormControl>
+            <FormLabel>FitnessFunctions</FormLabel>
+            <RadioGroup
+              value={selectedFitnessFunction}
+              onChange={(event) => {
+                setSelectedFitnessFunction(event.target.value);
+              }}
+            >
+              {fitnessFunctions.map((fitnessFunction) => {
+                return (
+                  <FormControlLabel
+                    value={fitnessFunction.id}
+                    control={<Radio />}
+                    label={fitnessFunction.name}
+                  />
+                );
+              })}
+            </RadioGroup>
+          </FormControl>
+        </CardContent>
+      </Card>
+    );
+  };
+
+  const sendRequest = async () => {
+    await api
+      .post("http://localhost:3001/api/insert", {
+        name: "a4",
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
   useEffect(() => {
     fetchAlgorithms();
-    fetchTestingFunctions();
+    fetchFitnessFunction();
   }, []);
 
   return (
-    <div className="App">
-      <section className="algorithms">
-        <h2>Algorithms</h2>
-        {renderAlgorithms()}
-      </section>
-      <section className="testingFunctions">
-        <h2>Testing Functions</h2>
-        {renderTestingFunctions()}
-      </section>
-      <button>Go</button>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container style={{ marginTop: theme.spacing(2) }}>
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                {renderAlgorithms()}
+              </Grid>
+              <Grid item xs={12}>
+                {renderFitnessFunctions()}
+              </Grid>
+              <Grid item xs={12}>
+                <Button variant="outlined" onClick={sendRequest} size="large">
+                  Send
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={9}>
+            <Card>
+              <CardContent>Result</CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
