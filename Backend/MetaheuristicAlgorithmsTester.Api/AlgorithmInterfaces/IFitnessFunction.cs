c2006cde65b90bb2f2f1e7ee3867fd8ca4f17157
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-01-07 23:35:53 +0100

    added NumberOfParameters

--- Backend/MetaheuristicAlgorithmsTester.Api/AlgorithmInterfaces/IFitnessFunction.cs
+++ Backend/MetaheuristicAlgorithmsTester.Api/AlgorithmInterfaces/IFitnessFunction.cs
@@ -5,5 +5,14 @@
         double Function(params double[] x);
         double[] LowerBounds { get; set; }
         double[] UpperBounds { get; set; }
+
+        /// <summary>
+        /// Gets or sets the number of parameters for the fitness function.
+        /// </summary>
+        /// <remarks>
+        /// This property represents the number of parameters accepted by the fitness function.
+        /// If the fitness function accepts an infinite number of parameters, set this value to 0.
+        /// </remarks>
+        public int NumberOfParameters { get; set; }
     }
 }
