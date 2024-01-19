e449a0205162e0ae0dbd8df8452ef8fcef37a652
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2024-01-19 10:27:09 +0100

    added dimensions

--- Backend/MetaheuristicAlgorithmsTester.Api/MetaheuristicAlgorithmsTester.Domain/Entities/FitnessFunction.cs
+++ Backend/MetaheuristicAlgorithmsTester.Api/MetaheuristicAlgorithmsTester.Domain/Entities/FitnessFunction.cs
@@ -8,7 +8,7 @@ namespace MetaheuristicAlgorithmsTester.Domain.Entities
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public string FileName { get; set; } = default!;
-
+        public int NumberOfParameters { get; set; }
         [NotMapped]
         public byte[]? DllFileBytes { get; set; }
     }
---
+++ Backend/MetaheuristicAlgorithmsTester.Api/MetaheuristicAlgorithmsTester.Infrastracture/Migrations/20240118025956_AddedNumbersOfParameters.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetaheuristicAlgorithmsTester.Infrastracture.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNumbersOfParameters : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfParameters",
+                table: "FitnessFunctions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfParameters",
+                table: "FitnessFunctions");
+        }
+    }
+}
